66e3b8106ee4a9ed9f5cf614000e667277b1f200
RavenbornJB <blackstarjeremy03@gmail.com>
2021-03-14 14:11:07 +0200

    Started GameManager functionality

--- src/GameManager.h
+++ src/GameManager.h
@@ -5,12 +5,18 @@
 #ifndef TICTACTOE_SIM_GAMEMANAGER_H
 #define TICTACTOE_SIM_GAMEMANAGER_H
 
+#include <string>
 
+#include "Board.h"
+#include "Player.h"
 
 class GameManager {
+    private:
+        static std::pair<std::string, std::string> get_player_type_strings();
 
+    public:
+        void play();
 };
-
 
 
 #endif //TICTACTOE_SIM_GAMEMANAGER_H
