2b49ccc0251afdc9b8943870e32173f2e509f83f
RavenbornJB <blackstarjeremy03@gmail.com>
2021-03-14 16:10:31 +0200

    yeah so stupid i'll just redo this here

---
+++ src/GameManager.h
@@ -0,0 +1,14 @@
+//
+// Created by black on 14-Mar-21.
+//
+
+#ifndef TICTACTOE_SIM_GAMEMANAGER_H
+#define TICTACTOE_SIM_GAMEMANAGER_H
+
+
+class GameManager {
+
+};
+
+
+#endif //TICTACTOE_SIM_GAMEMANAGER_H
